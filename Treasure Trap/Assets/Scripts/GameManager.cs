1a5de2c02e698150a5c0ea702adb9ef61325f2a3
Garrett Harr <garrett.harr27@gmail.com>
2023-02-08 00:36:48 -0600

    Fix Bug in Update
    Fixed a bug that wasn't allowing the player to give any input

--- Treasure Trap/Assets/Scripts/GameManager.cs
+++ Treasure Trap/Assets/Scripts/GameManager.cs
@@ -60,7 +60,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isWin)
+        if (!isWin)
         {
 
             //Placing A Piece==================================
