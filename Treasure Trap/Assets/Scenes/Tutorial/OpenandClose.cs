502572f1dfe518efcec79f255e87e8eead7d4c73
Awpace1 <103952273+Awpace1@users.noreply.github.com>
2023-03-10 15:55:00 -0600

    Rules List 1

--- Treasure Trap/Assets/Scenes/Tutorial/OpenandClose.cs
+++ Treasure Trap/Assets/Scenes/Tutorial/OpenandClose.cs
@@ -34,6 +34,14 @@ public class OpenandClose : MonoBehaviour
         RulePages[0].SetActive(true);
         RulePages[1].SetActive(false);
         RulePages[2].SetActive(false);
+        RulePages[3].SetActive(false);
+        RulePages[4].SetActive(false);
+        RulePages[5].SetActive(false);
+        RulePages[6].SetActive(false);
+        RulePages[7].SetActive(false);
+        RulePages[8].SetActive(false);
+        RulePages[9].SetActive(false);
+        RulePages[10].SetActive(false);
     }
 
     public void OpenRule2()
@@ -41,6 +49,14 @@ public class OpenandClose : MonoBehaviour
         RulePages[1].SetActive(true);
         RulePages[0].SetActive(false);
         RulePages[2].SetActive(false);
+        RulePages[3].SetActive(false);
+        RulePages[4].SetActive(false);
+        RulePages[5].SetActive(false);
+        RulePages[6].SetActive(false);
+        RulePages[7].SetActive(false);
+        RulePages[8].SetActive(false);
+        RulePages[9].SetActive(false);
+        RulePages[10].SetActive(false);
     }
 
     public void OpenRule3()
@@ -48,5 +64,133 @@ public class OpenandClose : MonoBehaviour
         RulePages[2].SetActive(true);
         RulePages[0].SetActive(false);
         RulePages[1].SetActive(false);
+        RulePages[3].SetActive(false);
+        RulePages[4].SetActive(false);
+        RulePages[5].SetActive(false);
+        RulePages[6].SetActive(false);
+        RulePages[7].SetActive(false);
+        RulePages[8].SetActive(false);
+        RulePages[9].SetActive(false);
+        RulePages[10].SetActive(false);
+    }
+
+    public void OpenRule4()
+    {
+        RulePages[3].SetActive(true);
+        RulePages[0].SetActive(false);
+        RulePages[1].SetActive(false);
+        RulePages[2].SetActive(false);
+        RulePages[4].SetActive(false);
+        RulePages[5].SetActive(false);
+        RulePages[6].SetActive(false);
+        RulePages[7].SetActive(false);
+        RulePages[8].SetActive(false);
+        RulePages[9].SetActive(false);
+        RulePages[10].SetActive(false);
+    }
+
+    public void OpenRule5()
+    {
+        RulePages[4].SetActive(true);
+        RulePages[0].SetActive(false);
+        RulePages[1].SetActive(false);
+        RulePages[3].SetActive(false);
+        RulePages[2].SetActive(false);
+        RulePages[5].SetActive(false);
+        RulePages[6].SetActive(false);
+        RulePages[7].SetActive(false);
+        RulePages[8].SetActive(false);
+        RulePages[9].SetActive(false);
+        RulePages[10].SetActive(false);
+    }
+
+    public void OpenRule6()
+    {
+        RulePages[5].SetActive(true);
+        RulePages[0].SetActive(false);
+        RulePages[1].SetActive(false);
+        RulePages[3].SetActive(false);
+        RulePages[4].SetActive(false);
+        RulePages[2].SetActive(false);
+        RulePages[6].SetActive(false);
+        RulePages[7].SetActive(false);
+        RulePages[8].SetActive(false);
+        RulePages[9].SetActive(false);
+        RulePages[10].SetActive(false);
+    }
+
+    public void OpenRule7()
+    {
+        RulePages[6].SetActive(true);
+        RulePages[0].SetActive(false);
+        RulePages[1].SetActive(false);
+        RulePages[3].SetActive(false);
+        RulePages[4].SetActive(false);
+        RulePages[5].SetActive(false);
+        RulePages[2].SetActive(false);
+        RulePages[7].SetActive(false);
+        RulePages[8].SetActive(false);
+        RulePages[9].SetActive(false);
+        RulePages[10].SetActive(false);
+    }
+
+    public void OpenRule8()
+    {
+        RulePages[7].SetActive(true);
+        RulePages[0].SetActive(false);
+        RulePages[1].SetActive(false);
+        RulePages[3].SetActive(false);
+        RulePages[4].SetActive(false);
+        RulePages[5].SetActive(false);
+        RulePages[6].SetActive(false);
+        RulePages[2].SetActive(false);
+        RulePages[8].SetActive(false);
+        RulePages[9].SetActive(false);
+        RulePages[10].SetActive(false);
+    }
+
+    public void OpenRule9()
+    {
+        RulePages[8].SetActive(true);
+        RulePages[0].SetActive(false);
+        RulePages[1].SetActive(false);
+        RulePages[3].SetActive(false);
+        RulePages[4].SetActive(false);
+        RulePages[5].SetActive(false);
+        RulePages[6].SetActive(false);
+        RulePages[7].SetActive(false);
+        RulePages[2].SetActive(false);
+        RulePages[9].SetActive(false);
+        RulePages[10].SetActive(false);
+    }
+
+    public void OpenRule10()
+    {
+        RulePages[9].SetActive(true);
+        RulePages[0].SetActive(false);
+        RulePages[1].SetActive(false);
+        RulePages[3].SetActive(false);
+        RulePages[4].SetActive(false);
+        RulePages[5].SetActive(false);
+        RulePages[6].SetActive(false);
+        RulePages[7].SetActive(false);
+        RulePages[8].SetActive(false);
+        RulePages[2].SetActive(false);
+        RulePages[10].SetActive(false);
+    }
+
+    public void OpenRule11()
+    {
+        RulePages[10].SetActive(true);
+        RulePages[0].SetActive(false);
+        RulePages[1].SetActive(false);
+        RulePages[3].SetActive(false);
+        RulePages[4].SetActive(false);
+        RulePages[5].SetActive(false);
+        RulePages[6].SetActive(false);
+        RulePages[7].SetActive(false);
+        RulePages[8].SetActive(false);
+        RulePages[9].SetActive(false);
+        RulePages[2].SetActive(false);
     }
 }
