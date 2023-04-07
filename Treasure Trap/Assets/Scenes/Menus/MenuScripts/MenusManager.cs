1cd5a362fc9700537ec4b3dbaa0aff0851b28c52
Edwin Morataya <emorataya@harding.edu>
2023-04-06 20:24:29 -0500

    Clear playerprefs when closing game

--- Treasure Trap/Assets/Scenes/Menus/MenuScripts/MenusManager.cs
+++ Treasure Trap/Assets/Scenes/Menus/MenuScripts/MenusManager.cs
@@ -77,6 +77,7 @@ public class MenusManager : MonoBehaviour
     public void ExitGame()
     {
         Debug.Log("Your Application is closed");
+        PlayerPrefs.DeleteAll();
         Application.Quit();
     }
 
